b07c972432b5ebc94cbc86aa37fa37e4d572b3f8
FlutLab.io <>
2022-02-19 15:04:59 +0000

    Autosave

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -12,46 +12,49 @@ class HomeState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text('Contacts'),
+        ),
         drawer: Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text('Меню'),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Меню'),
+              ),
+              ListTile(
+                title: const Text('Мій профіль'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Вийти'),
+                onTap: () {
+                  AlertDialog(
+                    title: Text('Reset settings?'),
+                    content: Text('This will reset your device to its default factory settings.'),
+                    actions: [
+                      FlatButton(
+                        textColor: Color(0xFF6200EE),
+                        onPressed: () {},
+                        child: Text('CANCEL'),
+                      ),
+                      FlatButton(
+                        textColor: Color(0xFF6200EE),
+                        onPressed: () {},
+                        child: Text('ACCEPT'),
+                      ),
+                    ],
+                  );
+                  Navigator.pop(context);
+                },
+              ),
+            ],
           ),
-          ListTile(
-            title: const Text('Мій профіль'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: const Text('Вийти'),
-            onTap: () {
-              AlertDialog(
-                title: Text('Reset settings?'),
-                content: Text('This will reset your device to its default factory settings.'),
-                actions: [
-                  FlatButton(
-                    textColor: Color(0xFF6200EE),
-                    onPressed: () {},
-                    child: Text('CANCEL'),
-                  ),
-                  FlatButton(
-                    textColor: Color(0xFF6200EE),
-                    onPressed: () {},
-                    child: Text('ACCEPT'),
-                  ),
-                ],
-              );
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      ),
-    ));
+        ));
   }
 }
