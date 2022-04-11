0cb1cece666819172b929ed06a18c45ac019be34
FlutLab.io <>
2022-04-11 20:51:44 +0000

    Autosave

--- lib/table_calendar.dart
+++ lib/table_calendar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:table_calendar/table_calendar.dart';.
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class ScheduleTable extends StatefulWidget {
   @override
