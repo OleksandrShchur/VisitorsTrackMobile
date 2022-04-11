8a9a8b705ff9cc2ddf16bbe9dbb5703df9b5a6ca
FlutLab.io <>
2022-04-11 20:39:58 +0000

    Autosave

--- lib/table_calendar.dart
+++ lib/table_calendar.dart
@@ -49,6 +49,7 @@ class _ScheduleTableState extends State<ScheduleTable> {
             onPageChanged: (focusedDay) {
               _focusedDay = focusedDay;
             },
+            locale: 'ua_UA',
           ),
         ],
       ),
