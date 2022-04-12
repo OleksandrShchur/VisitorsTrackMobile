7de8438f198902fdfd1d9fe14ff5d11bc4452ef1
FlutLab.io <>
2022-04-12 18:25:53 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/login_widget.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() => runApp(MyApp());
+void main() => initializeDateFormatting().then((_) => runApp(MyApp())));
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
