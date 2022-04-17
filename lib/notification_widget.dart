a7ea52fc8ca0be510c4ac43e75ff7d84825c4c16
FlutLab.io <>
2022-04-17 19:20:58 +0000

    Autosave

--- lib/notification_widget.dart
+++ lib/notification_widget.dart
@@ -78,7 +78,7 @@ class ListItemWidget extends State<SwipeList> {
                   ),
                   Expanded(
                     child: Container(
-                      height: 100,
+                      //height: 100,
                       child: Padding(
                         padding: EdgeInsets.fromLTRB(10, 2, 0, 0),
                         child: Column(
