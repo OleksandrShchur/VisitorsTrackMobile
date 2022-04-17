f7d6747f9eb177a3739257a1f925fdebe776e27c
FlutLab.io <>
2022-04-17 18:01:37 +0000

    Autosave

--- lib/group_widget.dart
+++ lib/group_widget.dart
@@ -23,42 +23,42 @@ class _GroupTableState extends State<GroupTable> {
       appBar: AppBar(
         title: Text('Список групи'),
       ),
-      body: Column(
-        children: [
-          // Card(
-          //   child: Column(
-          //     mainAxisSize: MainAxisSize.min,
-          //     children: <Widget>[
-          //       const ListTile(
-          //         leading: Icon(Icons.album),
-          //         title: Text('The Enchanted Nightingale'),
-          //         subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-          //       ),
-          //       Row(
-          //         mainAxisAlignment: MainAxisAlignment.end,
-          //         children: <Widget>[
-          //           TextButton(
-          //             child: const Text('BUY TICKETS'),
-          //             onPressed: () {
-          //               /* ... */
-          //             },
-          //           ),
-          //           const SizedBox(width: 8),
-          //           TextButton(
-          //             child: const Text('LISTEN'),
-          //             onPressed: () {
-          //               /* ... */
-          //             },
-          //           ),
-          //           const SizedBox(width: 8),
-          //         ],
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          GroupList(),
-        ],
-      ),
+      body: GroupList(),
+      // Column(
+      //   children: [
+      //     // Card(
+      //     //   child: Column(
+      //     //     mainAxisSize: MainAxisSize.min,
+      //     //     children: <Widget>[
+      //     //       const ListTile(
+      //     //         leading: Icon(Icons.album),
+      //     //         title: Text('The Enchanted Nightingale'),
+      //     //         subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+      //     //       ),
+      //     //       Row(
+      //     //         mainAxisAlignment: MainAxisAlignment.end,
+      //     //         children: <Widget>[
+      //     //           TextButton(
+      //     //             child: const Text('BUY TICKETS'),
+      //     //             onPressed: () {
+      //     //               /* ... */
+      //     //             },
+      //     //           ),
+      //     //           const SizedBox(width: 8),
+      //     //           TextButton(
+      //     //             child: const Text('LISTEN'),
+      //     //             onPressed: () {
+      //     //               /* ... */
+      //     //             },
+      //     //           ),
+      //     //           const SizedBox(width: 8),
+      //     //         ],
+      //     //       ),
+      //     //     ],
+      //     //   ),
+      //     // ),
+      //   ],
+      // ),
     );
   }
 }
