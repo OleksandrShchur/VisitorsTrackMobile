f55da6fc75c2267862efcd151c8e46295c0a49ca
FlutLab.io <>
2022-04-20 18:59:29 +0000

    Autosave

--- lib/table_calendar.dart
+++ lib/table_calendar.dart
@@ -56,7 +56,7 @@ class _ScheduleTableState extends State<ScheduleTable> {
           // ),
           SfCalendar(
             view: CalendarView.month,
-            monthViewSettings: MonthViewSettings(showAgenda: true, navigationDirection: MonthNavigationDirection.vertical, numberOfWeeksInView: 3),
+            monthViewSettings: MonthViewSettings(showAgenda: true, navigationDirection: MonthNavigationDirection.vertical, numberOfWeeksInView: 2),
           ),
         ],
       ),
