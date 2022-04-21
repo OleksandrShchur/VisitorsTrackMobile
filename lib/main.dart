1ac43ed6bcc3ba2c70ac693e032dde10dbf24758
FlutLab.io <>
2022-04-21 19:06:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
+      // localizationsDelegates: [
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      // ],
       locale: const Locale('uk'),
       debugShowCheckedModeBanner: false,
       title: 'Visitors Tracker Mobile',
