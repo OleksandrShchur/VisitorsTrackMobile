e760cd223d9c664c4022c8ac99893479c25273b2
FlutLab.io <>
2022-04-21 18:59:56 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helloworld/login_widget.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
 
 void main() => initializeDateFormatting().then((_) => runApp(MyApp()));
 
@@ -8,6 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      supportedLocales: [
+        const Locale('uk'),
+      ],
       locale: const Locale('uk'),
       debugShowCheckedModeBanner: false,
       title: 'Visitors Tracker Mobile',
