cf5da3205f481bf1a6bf3198cd3e270276278852
FlutLab.io <>
2022-04-26 16:52:11 +0000

    Autosave

--- lib/daybook.dart
+++ lib/daybook.dart
@@ -33,8 +33,35 @@ class _DaybookState extends State<Daybook> {
         ),
         body: Column(
           children: [
-            Text(this.subjectName),
-            Text(this.date),
+            Center(
+              child: Card(
+                  child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.album),
+                  title: Text('The Enchanted Nightingale'),
+                  subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('BUY TICKETS'),
+                      onPressed: () {
+                        /* ... */
+                      },
+                    ),
+                    const SizedBox(width: 8),
+                    TextButton(
+                      child: const Text('LISTEN'),
+                      onPressed: () {
+                        /* ... */
+                      },
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                ),
+              ])),
+            )
           ],
         ));
   }
