49d379dc63583d5383f98c048a84d24ae8f1c347
FlutLab.io <>
2022-04-28 19:35:18 +0000

    Autosave

--- lib/profile_widget.dart
+++ lib/profile_widget.dart
@@ -35,7 +35,7 @@ class _UserProfileState extends State<UserProfile> {
               ),
             ),
             ListTile(
-              leading: Icon(Icons.account_box_outlined),
+              leading: Icon(Icons.account_box_outlined, size: 24),
               title: Text("Name of student"),
               subtitle: Text("Group of student"),
             ),
