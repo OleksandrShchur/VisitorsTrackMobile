88262ad4fa683b3a81b0e121e4b1699a6a9dba81
FlutLab.io <>
2022-04-28 19:30:55 +0000

    Autosave

--- lib/profile_widget.dart
+++ lib/profile_widget.dart
@@ -30,7 +30,7 @@ class _UserProfileState extends State<UserProfile> {
             Center(
                 child: CircleAvatar(
               backgroundColor: Colors.brown.shade800,
-              child: const Text('AH', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 32)),
+              child: const Text('AH', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 40)),
               minRadius: 80,
             ))
           ])),
